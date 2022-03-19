89a27b818dde30420f4118e479e12f411156e949
ETsBrother <etsbrother1@gmail.com>
2022-03-18 23:12:24 -0400

    final commit of 3/18

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-console.log('we did it');
-alert('do your hw');
-alert('it was on the master branch :pensive:');
+let percent = 50;
+let percent_str = '';
+const id = document.getElementById('Div');
+const moveUp = setInterval(antonios, 10);
+function antonios() {
+    percent -= 0.25;
+    percent_str = '' + percent + '%';
+    id.style.top = percent_str;
+    if (percent <= 15)
+        clearInterval(moveUp);
+}
+console.log('im a genius');
--- script.ts
+++ script.ts
@@ -1,3 +1,14 @@
-console.log('we did it');
-alert('do your hw');
-alert('it was on the master branch :pensive:');
+let percent: number = 50;
+let percent_str: string = '';
+const id = document.getElementById('Div');
+const moveUp = setInterval(antonios, 10);
+
+function antonios() {
+    percent -= 1;
+    percent_str = '' + percent + '%';
+    id.style.top = percent_str;
+    if(percent <= 10)
+        clearInterval(moveUp);
+}
+
+console.log('im a genius');
